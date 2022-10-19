b4055c4b4c534cd21bf4f13825b171e364bd872e
NachoMol <ignaciotmolina@gmail.com>
2022-10-19 19:40:15 -0300

    subiendo ejercicio 1 clase 2A

---
+++ clase2A_ejercicio1/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String cadena1 = "Hola";
+        String cadena2 = "Hola";
+
+        if (cadena1.compareTo(cadena2) == 0){
+            System.out.println("Son iguales");
+        }
+        else{
+            System.out.println("Son distintos");
+        }
+
+
+    }
+}

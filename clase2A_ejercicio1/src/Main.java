b3545033da0de02686e699e9a1cd40d5431dc529
NachoMol <ignaciotmolina@gmail.com>
2022-10-19 19:43:14 -0300

    agregando .equals

--- clase2A_ejercicio1/src/Main.java
+++ clase2A_ejercicio1/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         String cadena1 = "Hola";
-        String cadena2 = "Hola";
+        String cadena2 = "chau";
 
-        if (cadena1.compareTo(cadena2) == 0){
+        if (cadena1.equals(cadena2) == true){
             System.out.println("Son iguales");
         }
         else{
---
+++ clase2A_ejercicio2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer num1 = 1;
+        Integer num2 = 5;
+
+        if (num1.c)
+    }
+}

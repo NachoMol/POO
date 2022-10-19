6817f22182275729ded0e66aef461661f2e88539
NachoMol <ignaciotmolina@gmail.com>
2022-10-19 19:46:46 -0300

    subiendo clase 2A ejercicio 2

--- clase2A_ejercicio2/src/Main.java
+++ clase2A_ejercicio2/src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Integer num1 = 1;
-        Integer num2 = 5;
+        Integer num1 = 20;
+        Integer num2 = 20;
 
-        if (num1.c)
+        if (num1.equals(num2)){
+            System.out.println("son iguales");
+        }else if (num1.compareTo(num2) == -1){
+            System.out.println(num2 + " es el mayor, mientras que el menor es " + num1);
+        }else{
+            System.out.println(num1 + " es el mayor, mientras que el menor es " + num2);
+        }
     }
 }

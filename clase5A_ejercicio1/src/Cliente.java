fa52ab1ad421b99dfcc37b48e6b8522ca3bfa74a
NachoMol <ignaciotmolina@gmail.com>
2022-10-26 21:49:57 -0300

    agregando metodos a la clase cliente

--- clase5A_ejercicio1/src/Cliente.java
+++ clase5A_ejercicio1/src/Cliente.java
@@ -1,10 +1,22 @@
 public class Cliente {
     private String nombre;
+    private String apellido;
     private Integer numeroCliente;
-    private Double deuda;
+    private double deuda;
 
-    public Cliente(String nombre, Integer numeroCliente){
+    public Cliente(String nombre, String apellido, Integer numeroCliente){
         this.nombre = nombre;
+        this.apellido = apellido;
         this.numeroCliente = numeroCliente;
+        deuda=0;
     }
+
+    public void incrementarDeuda (double valor){
+        deuda += valor;
+    }
+
+    public void pagar(){
+        deuda= 0;
+    }
+
 }

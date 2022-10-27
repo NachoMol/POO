859b6f5ef2772d0a175e0fe5b78dad7d2093265b
NachoMol <ignaciotmolina@gmail.com>
2022-10-26 21:41:14 -0300

    creando constructor clase Cliente

---
+++ clase5A_ejercicio1/src/Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente {
+    private String nombre;
+    private Integer numeroCliente;
+    private Double deuda;
+
+    public Cliente(String nombre, Integer numeroCliente){
+        this.nombre = nombre;
+        this.numeroCliente = numeroCliente;
+    }
+}
--- clase5A_ejercicio1/src/cliente.java
+++
@@ -1,5 +0,0 @@
-public class cliente {
-    private String nombre;
-    private Integer numeroCliente;
-    private Double deuda;
-}

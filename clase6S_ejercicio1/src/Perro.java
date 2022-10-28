951544d1cf57579f432f263511579d4b1804e163
NachoMol <ignaciotmolina@gmail.com>
2022-10-27 22:22:40 -0300

    actualizando UML y creando clase perro

---
+++ clase6S_ejercicio1/src/Perro.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Perro {
+
+    private Boolean enAdopcion;
+    private String raza;
+    private LocalDate anioNacimiento;
+    private String peso;
+    private Boolean tieneChip;
+    private Boolean estaLastimado;
+    private String nombre;
+
+    public Perro (boolean enAdopcion, String raza, LocalDate anioNacimiento, String peso, Boolean tieneChip, Boolean estaLastimado, String nombre)
+
+
+}

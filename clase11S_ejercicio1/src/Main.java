90fa3d7839265aa1a1a4fb99da2710abe4cc8943
NachoMol <ignaciotmolina@gmail.com>
2022-11-10 22:50:31 -0300

    implementando polimorfismo a main

--- clase11S_ejercicio1/src/Main.java
+++ clase11S_ejercicio1/src/Main.java
@@ -7,8 +7,13 @@ public class Main {
         cliente1.extraer(600.0);
         System.out.println(cliente1.informarSaldo());
 
-        Cuenta cliente2 = new CuentaComitente(2,"Molina", 41710177,41710177, "asd1324")
+        Cuenta cliente2 = new CuentaComitente(2,"Molina", 41710177,41710177, "asd1324");
 
+        if(cliente2 instanceof CuentaComitente){
+            ((CuentaComitente)cliente2).depositar(5000.0);
+            ((CuentaComitente)cliente2).extraer(4500.0);
+            ((CuentaComitente)cliente2).extraer(4500.0,"asd1234");
+        }
 
     }
 }

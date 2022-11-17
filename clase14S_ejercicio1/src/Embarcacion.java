bc40c7783d1c99524a88222ccf0fbfb3ab85ee5d
NachoMol <ignaciotmolina@gmail.com>
2022-11-17 17:26:45 -0300

    agregando clases capitan y embarcacion, ademas corrijo atributo anioFabricacion

---
+++ clase14S_ejercicio1/src/Embarcacion.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+public class Embarcacion {
+
+    private Capitan capitan;
+    private Double precioBase;
+    private Double valorAdicional;
+    private Integer anioFabricacion;
+
+    private Double esLora;
+
+    public Double calcularAlquiler(){
+        if(this.anioFabricacion > 2020){
+         return precioBase + valorAdicional;
+        }else{
+            return precioBase;
+        }
+    }
+
+
+
+
+}

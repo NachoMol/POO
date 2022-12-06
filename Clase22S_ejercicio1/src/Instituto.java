6f3b2f6f341dcc4a1e4383d7d0c47171635b52d7
NachoMol <ignaciotmolina@gmail.com>
2022-12-05 22:14:19 -0300

    subiendo Factory

--- Clase22S_ejercicio1/src/Instituto.java
+++ Clase22S_ejercicio1/src/Instituto.java
@@ -24,6 +24,6 @@ public class Instituto {
         return informacion;
     }
 
-    
+
 
 }
--- Clase22S_ejercicio1/src/OfertaFactory.java
+++ Clase22S_ejercicio1/src/OfertaFactory.java
@@ -1,2 +1,27 @@
 public class OfertaFactory {
+
+    private static OfertaFactory instancia;
+    public static final String CURSO="C";
+    public static final String PROGRAMA="P";
+
+    private OfertaFactory() {
+    }
+
+    public static OfertaFactory obtenerInstancia(){
+        if(instancia == null)
+            instancia= new OfertaFactory();
+        return instancia;
+    }
+
+    public OfertaAcademica crearOferta(String codigo, String nombre, String descripcion){
+        switch (codigo){
+            case "C":
+                return new Curso(nombre, descripcion);
+
+            case "P":
+                return new ProgramaIntensivo(nombre, descripcion);
+
+        }
+        return null;
+    }
 }
